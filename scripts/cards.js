99134361d4f1b0fb572d20c510b1714db5b20413
Danil <danilcaheleh1@gmail.com>
2024-06-06 15:44:49 +0300

    firstVersion

--- scripts/cards.js
+++ scripts/cards.js
@@ -1,11 +1,11 @@
 const initialCards = [
     {
-      name: "Архыз",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/arkhyz.jpg",
+      name: "Лондон",
+      link: "https://images.unsplash.com/photo-1716220557737-f2616bd6bb37?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
-      name: "Челябинская область",
-      link: "https://pictures.s3.yandex.net/frontend-developer/cards-compressed/chelyabinsk-oblast.jpg",
+      name: "Нью-Йорк",
+      link: "https://images.unsplash.com/photo-1715694290840-bd862815c8ba?q=80&w=1964&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
     },
     {
       name: "Иваново",
--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,31 @@
 // @todo: Темплейт карточки
+let tempCard = document.getElementById('card-template').content;
 
 // @todo: DOM узлы
+let placesList = document.querySelector('.places__list');
 
 // @todo: Функция создания карточки
-
+function addCard (card, delButton) {
+    // Клонировали шаблон из темплейта
+    let itemClone = tempCard.querySelector('.places__item').cloneNode(true);
+    // Переменные вложенных элементов
+    let cardImg = itemClone.querySelector('.card__image');
+    let cardTitle = itemClone.querySelector('.card__title');
+    let cardDel = itemClone.querySelector('.card__delete-button');
+    // Установили значения вложенных элементов
+    cardImg.src = card.link;
+    cardTitle.textContent = card.name;
+    // Обработчик удаления карточки
+    cardDel.addEventListener('click', delButton);
+    // Возврат значения функции
+    return itemClone;
+}
 // @todo: Функция удаления карточки
+function delCard (evt) {
+   evt.target.closest('.places__item').remove();
+}
 
 // @todo: Вывести карточки на страницу
+initialCards.forEach(function(item) {
+    placesList.append(addCard(item, delCard))
+});
